131a055208afc92803a9b5024d0e76c50bbaf56d
0xaa4eb <37665782+0xaa4eb@users.noreply.github.com>
2021-08-17 20:38:01 +0300

    Use listof

--- ulyp-ui/src/main/kotlin/com/ulyp/ui/renderers/RenderedEnum.kt
+++ ulyp-ui/src/main/kotlin/com/ulyp/ui/renderers/RenderedEnum.kt
@@ -5,15 +5,15 @@ import com.ulyp.ui.RenderSettings
 import com.ulyp.ui.util.ClassNameUtils.toSimpleName
 import com.ulyp.ui.util.CssClass
 import com.ulyp.ui.util.StyledText.of
-import java.util.*
 
 class RenderedEnum(representation: EnumRepresentation, renderSettings: RenderSettings) :
     RenderedObject(representation.type) {
     init {
-        val className =
-            if (renderSettings.showTypes()) representation.type.name else toSimpleName(representation.type.name)
+
+        val className = if (renderSettings.showTypes()) representation.type.name else toSimpleName(representation.type.name)
+
         super.getChildren().addAll(
-            Arrays.asList(
+            listOf(
                 of(className, CssClass.CALL_TREE_TYPE_NAME),
                 of(".", CssClass.CALL_TREE_PLAIN_TEXT),
                 of(representation.name, CssClass.CALL_TREE_PLAIN_TEXT)
