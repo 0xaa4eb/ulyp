d8f7f8bd33213bae6e1fbf72b03127678823fedb
Andrei <37665782+0xaa4eb@users.noreply.github.com>
2022-01-24 18:58:18 +0400

    Naming

--- ulyp-agent-classes/src/test/java/com/test/cases/ConcurrentRecordingTest.java
+++ ulyp-agent-classes/src/test/java/com/test/cases/ConcurrentRecordingTest.java
@@ -9,7 +9,7 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 
-public class ConcurretRecordingTest extends AbstractInstrumentationTest {
+public class ConcurrentRecordingTest extends AbstractInstrumentationTest {
 
     @Test
     public void shouldRecordInConcurrentMode() {
