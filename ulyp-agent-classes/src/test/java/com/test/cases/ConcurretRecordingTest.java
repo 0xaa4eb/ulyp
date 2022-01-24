9cb505ace3f4af079e85c6e25dba2557ab0791eb
Andrei <37665782+0xaa4eb@users.noreply.github.com>
2022-01-24 18:58:07 +0400

    Concurrent recording test

---
+++ ulyp-agent-classes/src/test/java/com/test/cases/ConcurretRecordingTest.java
@@ -0,0 +1,49 @@
+package com.test.cases;
+
+import com.test.cases.util.ForkProcessBuilder;
+import com.test.cases.util.RecordingResult;
+import com.ulyp.core.util.MethodMatcher;
+import org.junit.Test;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+public class ConcurretRecordingTest extends AbstractInstrumentationTest {
+
+    @Test
+    public void shouldRecordInConcurrentMode() {
+        RecordingResult recordingResult = runForkProcess(
+                new ForkProcessBuilder()
+                        .setMainClassName(MultithreadedExample.class)
+                        .setMethodToRecord(MethodMatcher.parse("*.*"))
+                        .setLogLevel("OFF")
+        );
+
+
+        recordingResult.assertRecordingSessionCount(10001);
+    }
+
+    public static class MultithreadedExample {
+
+        public static void main(String[] args) throws InterruptedException {
+            ExecutorService executorService = Executors.newFixedThreadPool(5);
+
+            for (int i = 0; i < 10000; i++) {
+                executorService.submit(
+                        () -> new Clazz().bar()
+                );
+            }
+
+            executorService.shutdown();
+            executorService.awaitTermination(1, TimeUnit.MINUTES);
+        }
+    }
+
+    public static class Clazz {
+
+        public int bar() {
+            return 1;
+        }
+    }
+}
