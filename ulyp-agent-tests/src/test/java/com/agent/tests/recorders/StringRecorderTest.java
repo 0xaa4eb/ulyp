1966ba4924451e553046ac37c01896e766d74782
Andrey <37665782+0xaa4eb@users.noreply.github.com>
2022-05-19 15:24:51 +0400

    Add tests

--- ulyp-agent-tests/src/test/java/com/agent/tests/recorders/StringRecorderTest.java
+++ ulyp-agent-tests/src/test/java/com/agent/tests/recorders/StringRecorderTest.java
@@ -4,14 +4,20 @@ import com.agent.tests.util.AbstractInstrumentationTest;
 import com.agent.tests.util.ForkProcessBuilder;
 import com.ulyp.core.recorders.StringObjectRecord;
 import com.ulyp.storage.CallRecord;
+import org.hamcrest.Matchers;
 import org.junit.Test;
 
+import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.lessThan;
 import static org.junit.Assert.assertThat;
 
 public class StringRecorderTest extends AbstractInstrumentationTest {
 
-    public static class TestCases {
+    public static class StringTestCases {
+
+        public static String returnShortString() {
+            return "ABCDEF";
+        }
 
         public static String returnLongString() {
             StringBuilder builder = new StringBuilder();
@@ -23,13 +29,28 @@ public class StringRecorderTest extends AbstractInstrumentationTest {
 
         public static void main(String[] args) {
             System.out.println(returnLongString());
+            System.out.println(returnShortString());
         }
+    }
+
+    @Test
+    public void shouldRecordShortString() {
+        CallRecord root = run(
+                new ForkProcessBuilder()
+                        .withMainClassName(StringTestCases.class)
+                        .withMethodToRecord("returnShortString")
+        );
+
+        StringObjectRecord returnValue = (StringObjectRecord) root.getReturnValue();
+        assertThat(returnValue.value(), is("ABCDEF"));
     }
 
     @Test
     public void shouldCutLongStringWhileRecording() {
         CallRecord root = run(
-                new ForkProcessBuilder().withMainClassName(TestCases.class).withMethodToRecord("returnLongString")
+                new ForkProcessBuilder()
+                        .withMainClassName(StringTestCases.class)
+                        .withMethodToRecord("returnLongString")
         );
 
         StringObjectRecord returnValue = (StringObjectRecord) root.getReturnValue();
--- ulyp-common/src/main/java/com/ulyp/core/recorders/ObjectRecorderRegistry.java
+++ ulyp-common/src/main/java/com/ulyp/core/recorders/ObjectRecorderRegistry.java
@@ -34,7 +34,7 @@ public enum ObjectRecorderRegistry {
     FILE_RECORDER(new FileRecorder((byte) 26), 90),
     PATH_RECORDER(new PathRecorder((byte) 27), 90),
     DATE_RECORDER(new DateRecorder((byte) 20), 90),
-    TO_STRING_RECORDER(new ToStringRecorder((byte) 91), 99),
+    TO_STRING_RECORDER(new ToStringPrintingRecorder((byte) 91), 99),
     IDENTITY_RECORDER(new IdentityRecorder((byte) 0), Integer.MAX_VALUE / 2),
     // Null recorder is only used manually, so it has max available order
     NULL_RECORDER(new NullObjectRecorder((byte) 9), Integer.MAX_VALUE);
