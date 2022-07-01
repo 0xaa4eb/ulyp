4be94747eeb9420bd34a3aa653ac0ba88cfeaf00
Andrey <37665782+0xaa4eb@users.noreply.github.com>
2022-07-01 14:49:48 +0400

    Cleanup AntPathMatcher

--- ulyp-common/src/test/java/com/ulyp/core/util/ClassMatcherTest.java
+++ ulyp-common/src/test/java/com/ulyp/core/util/ClassMatcherTest.java
@@ -12,8 +12,6 @@ public class ClassMatcherTest {
         Type type = Type.builder().name("com.pckg.SomeClass").build();
 
         assertTrue(ClassMatcher.parse("*").matches(type));
-
-//        assertTrue(ClassMatcher.parse("**").matches(type));
     }
 
     @Test
