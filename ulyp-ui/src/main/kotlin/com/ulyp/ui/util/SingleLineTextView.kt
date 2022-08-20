4b4fa328d6c666a57180fce307ef9ef499ec0129
Andrei Cheboksarov <37665782+0xaa4eb@users.noreply.github.com>
2022-08-20 14:31:51 +0300

    Trim strings and show full text on hover

---
+++ ulyp-ui/src/main/kotlin/com/ulyp/ui/util/SingleLineTextView.kt
@@ -0,0 +1,17 @@
+package com.ulyp.ui.util
+
+import javafx.scene.text.Text
+
+class TrimmedTextView(value: String) : Text() {
+
+    init {
+        var trimmed = value
+                .replace("\t", "\\t")
+                .replace("\r", "\\r")
+                .replace("\n", "\\n")
+        if (trimmed.length > 50) {
+            trimmed = trimmed.substring(0..50) + "..."
+        }
+        this.text = trimmed
+    }
+}
