295a5510704e63bc43f8366f99b582eb0b11d5bb
Andrei Cheboksarov <37665782+0xaa4eb@users.noreply.github.com>
2023-11-04 20:54:41 +0300

    Fix some issues

---
+++ ulyp-agent-api/src/main/java/com/ulyp/agent/ApiException.java
@@ -0,0 +1,10 @@
+package com.ulyp.agent;
+
+import com.ulyp.core.exception.UlypException;
+
+public class ApiException extends UlypException {
+
+    public ApiException(Throwable cause) {
+        super(cause);
+    }
+}
