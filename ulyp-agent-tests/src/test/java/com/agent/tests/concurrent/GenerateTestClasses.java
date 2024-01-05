6fa98a2eae38c9442ba244c410669005c74baa45
Andrei Cheboksarov <37665782+0xaa4eb@users.noreply.github.com>
2024-01-05 16:28:18 +0400

    Better generated and larger generated classes for benchmarks

--- ulyp-agent-tests/src/test/java/com/agent/tests/concurrent/GenerateTestClasses.java
+++ ulyp-agent-tests/src/test/java/com/agent/tests/concurrent/GenerateTestClasses.java
@@ -16,7 +16,7 @@ public class GenerateTestClasses {
         }
         String pckg = args[1];
 
-        for (int i = 0; i <= 1000; i++) {
+        for (int i = 0; i <= 100; i++) {
             String name = "X" + i;
 
             try(BufferedWriter writer = new BufferedWriter(new FileWriter(Paths.get(pathStr, name + ".java").toFile()))) {
@@ -24,32 +24,57 @@ public class GenerateTestClasses {
                 writer.newLine();
                 writer.append("// Automatically generated and only used for tests");
                 writer.newLine();
+                writer.append("@SuppressWarnings(\"unused\")");
+                writer.newLine();
                 writer.append("public class ").append(name).append(" {");
                 writer.newLine();
-                appendMethod(writer, "vgkdsdfg");
-                appendMethod(writer, "gkfjdyfw");
-                appendMethod(writer, "fdkjskjw");
-                appendMethod(writer, "fdkjshfr");
-                appendMethod(writer, "ffdjshfe");
-                appendMethod(writer, "fdkjshdf");
-                appendMethod(writer, "mfjcvghs");
-                appendMethod(writer, "uioocxvf");
-                appendMethod(writer, "lkhgjhds");
-                appendMethod(writer, "vcmxnmdf");
-                appendMethod(writer, "mfdsjdhs");
-                appendMethod(writer, "go8udsuy");
+                appendStringArgsMethod(writer, "m1");
+                appendStringArgsMethod(writer, "m2");
+                appendStringArgsMethod(writer, "m3");
+                appendStringArgsMethod(writer, "m4");
+                appendStringArgsMethod(writer, "m5");
+                appendIntArgsMethod(writer, "m8");
+                appendIntArgsMethod(writer, "m9");
+                appendIntArgsMethod(writer, "m10");
+                appendIntArgsMethod(writer, "m11");
+                appendIntArgsMethod(writer, "m12");
+                appendSimpleMethod(writer, "m13");
+                appendSimpleMethod(writer, "m14");
+                appendSimpleMethod(writer, "m15");
+                appendSimpleMethod(writer, "m6");
+                appendSimpleMethod(writer, "m7");
                 writer.append("}");
                 writer.newLine();
             }
         }
     }
 
-    private static void appendMethod(BufferedWriter writer, String methodName) throws IOException {
-        writer.append("\tpublic String ").append(methodName).append("() {");
+    private static void appendSimpleMethod(BufferedWriter writer, String methodName) throws IOException {
+        writer.append("\tpublic void ").append(methodName).append("() {");
         writer.newLine();
         writer.append("\t\tSystem.out.println(\"1\");");
         writer.newLine();
-        writer.append("\t\treturn \"1\";");
+        writer.append("\t}");
+        writer.newLine();
+    }
+
+    private static void appendStringArgsMethod(BufferedWriter writer, String methodName) throws IOException {
+        writer.append("\tpublic String ").append(methodName).append("(String a, String b, String c) {");
+        writer.newLine();
+        writer.append("\t\tSystem.out.println(\"1\");");
+        writer.newLine();
+        writer.append("\t\treturn a + b + c;");
+        writer.newLine();
+        writer.append("\t}");
+        writer.newLine();
+    }
+
+    private static void appendIntArgsMethod(BufferedWriter writer, String methodName) throws IOException {
+        writer.append("\tpublic String ").append(methodName).append("(Integer x, Long y, Long z) {");
+        writer.newLine();
+        writer.append("\t\tSystem.out.println(\"1\");");
+        writer.newLine();
+        writer.append("\t\treturn String.valueOf(y + z);");
         writer.newLine();
         writer.append("\t}");
         writer.newLine();
