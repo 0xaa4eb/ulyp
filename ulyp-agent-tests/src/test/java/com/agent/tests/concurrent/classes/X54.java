30e83dc4f0c07ca24a6b9ced49493f7ac5cda4ca
Andrey Cheboksarov <37665782+0xaa4eb@users.noreply.github.com>
2024-07-06 16:49:20 +0400

    Code analysis fixes (#138)

--- ulyp-agent-tests/src/test/java/com/agent/tests/concurrent/classes/X54.java
+++ ulyp-agent-tests/src/test/java/com/agent/tests/concurrent/classes/X54.java
@@ -1,60 +1,75 @@
 package com.agent.tests.concurrent.classes;
-// Automatically generated and only used for tests
+
+// Automatically generated and only used for instrumentation tests
 @SuppressWarnings("unused")
 public class X54 {
-	public String m1(String a, String b, String c) {
-		System.out.println("1");
-		return a + b + c;
-	}
-	public String m2(String a, String b, String c) {
-		System.out.println("1");
-		return a + b + c;
-	}
-	public String m3(String a, String b, String c) {
-		System.out.println("1");
-		return a + b + c;
-	}
-	public String m4(String a, String b, String c) {
-		System.out.println("1");
-		return a + b + c;
-	}
-	public String m5(String a, String b, String c) {
-		System.out.println("1");
-		return a + b + c;
-	}
-	public String m8(Integer x, Long y, Long z) {
-		System.out.println("1");
-		return String.valueOf(y + z);
-	}
-	public String m9(Integer x, Long y, Long z) {
-		System.out.println("1");
-		return String.valueOf(y + z);
-	}
-	public String m10(Integer x, Long y, Long z) {
-		System.out.println("1");
-		return String.valueOf(y + z);
-	}
-	public String m11(Integer x, Long y, Long z) {
-		System.out.println("1");
-		return String.valueOf(y + z);
-	}
-	public String m12(Integer x, Long y, Long z) {
-		System.out.println("1");
-		return String.valueOf(y + z);
-	}
-	public void m13() {
-		System.out.println("1");
-	}
-	public void m14() {
-		System.out.println("1");
-	}
-	public void m15() {
-		System.out.println("1");
-	}
-	public void m6() {
-		System.out.println("1");
-	}
-	public void m7() {
-		System.out.println("1");
-	}
+    public int m1(int a, int b, int c) {
+        System.out.println("1");
+        return a + b + c;
+    }
+
+    public float m2(float a, float b, float c) {
+        System.out.println("1");
+        return a + b + c;
+    }
+
+    public Object m3() {
+        System.out.println("1");
+        return new Object();
+    }
+
+    public float m4(float a, float b, float c, float d) {
+        System.out.println("1");
+        return a * b * c * d;
+    }
+
+    public int m5(short a, short b, short c) {
+        System.out.println("1");
+        return a + b + c;
+    }
+
+    public String m8(Integer x, Long y, Long z) {
+        System.out.println("1");
+        return String.valueOf(y + z);
+    }
+
+    public String m9(Integer x, Long y, Long z, String suffix) {
+        System.out.println("1");
+        return y + z + suffix;
+    }
+
+    public String m10(String prefix, Integer x, Long y, Long z, String suffix) {
+        System.out.println("1");
+        return prefix + y + z + suffix;
+    }
+
+    public String m11(Integer x, Long y, Long z) {
+        System.out.println("1");
+        return String.valueOf(y + z);
+    }
+
+    public String m12(Integer x, Long y, Long z) {
+        System.out.println("1");
+        return String.valueOf(y + z);
+    }
+
+    public void m13() {
+        System.out.println("hello");
+    }
+
+    public void m14() {
+        System.out.println("2");
+    }
+
+    public void m15() {
+        System.out.println("3");
+    }
+
+    public void m6() {
+        System.out.println("4");
+    }
+
+    public void m7() {
+        System.out.println("1");
+    }
 }
